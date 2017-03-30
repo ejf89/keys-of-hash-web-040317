ecd43c7112462f2fc642629e266b7de5204e85f8
Eric Farber <eric.j.farber@gmail.com>
2017-03-30 02:29:23 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,21 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+      returnArray = []
+
+      self.each do |k, v|
+          if v == arguments.join
+              returnArray.push(k)
+            # binding.pry
+        elsif v ==  arguments.join.to_i
+            returnArray.push(k)
+        elsif *v == arguments.join
+            # binding.pry
+            returnArray.push(k)
+        end
+      end
+    #   binding.pry
+    returnArray
   end
 end
